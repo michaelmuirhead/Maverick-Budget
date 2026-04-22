5f797d6a39e5c0450a377727af7b7f00bf98801d
michaelmuirhead <102689372+michaelmuirhead@users.noreply.github.com>
2026-04-22 17:00:56 -0500

    Delete app directory

--- app/(game)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { GameShell } from "@/components/layout/GameShell";
-
-export default function GameLayout({ children }: { children: React.ReactNode }) {
-  return <GameShell>{children}</GameShell>;
-}
